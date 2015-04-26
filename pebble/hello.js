8ef0c98fb6da57d89926c9536245220e95765b1b
elsapeng <elsa.peng@hotmail.com>
2015-04-26 12:58:58 -0400

    update main.c and hello.js

--- pebble/hello.js
+++ pebble/hello.js
@@ -2,6 +2,12 @@ var resume = "resume";
 var pause = "pause";
 var celsius = "celsius";
 var fahrenheit = "fahrenheit";
+var light = "light";
+var timer = "timer";
+var statc = "statc";
+var statf = "statf";
+var polling = "polling";
+
 
 Pebble.addEventListener("appmessage",
   function(e) {
@@ -14,11 +20,34 @@ Pebble.addEventListener("appmessage",
         sendToServer(celsius);
       } else if (e.payload.fahrenheit) {
         sendToServer(fahrenheit);
-      } 
-    }
+      } else if (e.payload.light) {
+        sendToServer(light);
+      } else if (e.payload.timer) {
+        sendToServer(timer);
+      } else if (e.payload.statc) {
+        sendToServer(statc);
+      } else if (e.payload.statf) {
+        sendToServer(statf);
+      } else if (e.payload.polling) {
+        sendToServer(polling);
+      }
+      }
 });
 
 // function sendToServer(part) {
+//   console.log(part);
+//   var req = new XMLHttpRequest();
+// 	var ipAddress = "158.130.109.148"; // Hard coded IP address
+// 	var port = "3001"; // Same port specified as argument to server
+// 	var url = "http://" + ipAddress + ":" + port + "/";
+// 	var method = "GET";
+// 	var async = true;
+//   console.log(url + part);
+//   Pebble.sendAppMessage({ "0": part});
+// }
+
+// function sendToServer2(part) {
+//   console.log(part);
 //   var req = new XMLHttpRequest();
 // 	var ipAddress = "158.130.109.148"; // Hard coded IP address
 // 	var port = "3001"; // Same port specified as argument to server
@@ -32,11 +61,17 @@ Pebble.addEventListener("appmessage",
 function sendToServer(part) {
 
 	var req = new XMLHttpRequest();
-	var ipAddress = "158.130.110.25"; // Hard coded IP address
-	var port = "3002"; // Same port specified as argument to server
+	var ipAddress = "165.123.216.173"; // Hard coded IP address
+	var port = "3001"; // Same port specified as argument to server
 	var url = "http://" + ipAddress + ":" + port + "/";
 	var method = "GET";
 	var async = true;
+  
+  req.onerror = function() {
+     Pebble.sendAppMessage( {
+      "0": "Connection Failed."
+    });
+  };
   
 	req.onload = function(e) {
                 var msg = "no response";
@@ -51,11 +86,45 @@ function sendToServer(part) {
                     else msg = "no message has been received!";
                 }
                 if (req.readyState == 4 && req.status == 200) {
+                   console.log(msg);
                    Pebble.sendAppMessage({ "0": msg });
+                   
                 } else {
                    Pebble.sendAppMessage({ "0": "Cannot connect to server."});
                 }      
 	};
   req.open(method, url + part, async);
+//   req.timeout = 3000;
   req.send(null);
 }
+
+// function sendToServer2(part) {
+
+// 	var req = new XMLHttpRequest();
+// 	var ipAddress = "165.123.216.173"; // Hard coded IP address
+// 	var port = "3001"; // Same port specified as argument to server
+// 	var url = "http://" + ipAddress + ":" + port + "/";
+// 	var method = "GET";
+// 	var async = true;
+  
+// 	req.onload = function(e) {
+//                    var msg = "no response";
+//                    msg = part;
+// //                 console.log(req.responseText);
+// //                 var response = JSON.parse(req.responseText);
+// //                 console.log(response);
+// //                 if (response) {
+// //                     if (response.name) {
+// //                         msg = response.name;
+// //                     }
+// //                     else msg = "no message has been received!";
+// //                 }
+                
+//                    Pebble.sendAppMessage({ "0": msg });
+           
+// 	};
+//   req.open(method, url + part, async);
+//   req.send(null);
+// }
+
+
--- pebble/main.c
+++ pebble/main.c
@@ -4,40 +4,63 @@ static Window *window;
 static TextLayer *hello_layer;
 static char msg[500];
 static int mode = 0;
+static int mode2 = 0;
+static int mode3 = 0;
+static int mode4 = 0;
 static int number_of_mode = 2;
+static int number_of_mode2 = 2;
+static int number_of_mode3 = 2;
+static int number_of_mode4 = 2;
 static int pause = 0;
 static int error_count = 0;
 static int last_time = 0;
+static int last_time2 = 0;
+static int sleeptime = 10000; // number of milliseconds to sleep
+static int ispolling = 0;
+
 
 
 //static const int CELSIUS = 0;
 //static const int FAHRENHEIT = 1;
+static const int POLL  = 12;
+static const int AVGC = 2;
+static const int AVGF = 3; 
+static const int TIMER = 6;
+static const int RGB = 5;
 static const int RESUME = 10;
 static const int PAUSE = 11;
 
 void resume();
 void topause();
 void send_req();
+void rgb();
+void polling();
+
+
+
+
+static void timer_callback(void *data) { 
+    text_layer_set_text_alignment(hello_layer, GTextAlignmentCenter);
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    if (ispolling) {
+      polling();
+      app_timer_register(sleeptime, timer_callback, NULL);
+    } else {
+      resume();
+    }
+}
 
 void out_sent_handler(DictionaryIterator *sent, void *context) {
    // outgoing message was delivered -- do nothing
+   
 }
 
 
 void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
-   // outgoing message failed
-   if (error_count > 10) {
-     error_count = 0;
-     text_layer_set_text(hello_layer, "Error out!");
-   } else {
-     error_count++;
-     if (last_time == 10)
-       resume();
-     if (last_time == 11)
-       topause();
-     if (last_time != 10 && last_time != 11)
-       send_req();
-   }   
+  
+   text_layer_set_text(hello_layer, "Error out!");
+
 }
 
 // Handles all messages received from server
@@ -53,8 +76,21 @@ void in_received_handler(DictionaryIterator *received, void *context) {
        strcpy(msg, text_tuple->value->cstring);
      }
      else strcpy(msg, "no value!");
-     
+     if (msg[0] == 'A' || msg[0] == 'P') {
+       int i = 0;
+       while (msg[i] != '\0') {
+         if (msg[i] == '#') {
+           msg[i] = '\n';
+         }
+         i++;
+       }
+     }
      text_layer_set_text(hello_layer, msg);
+     if (msg[0] == 'A') {
+       text_layer_set_font(hello_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+     } else {
+       text_layer_set_font(hello_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+     }
    }
    else {
      text_layer_set_text(hello_layer, "no message!");
@@ -75,6 +111,7 @@ void send_req() {
   int key;
   key = mode;
   last_time = key;
+  last_time2 = key;
   // send the message "hello?" to the phone, using key #0
   Tuplet value = TupletCString(key, "msg");
   dict_write_tuplet(iter, &value);
@@ -94,7 +131,24 @@ void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   send_req();
 }
 
-//exists the pause mode
+/* This called when select is long pressed. */
+void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+    int key = 0;
+    if (last_time2 == 0) {
+      key = AVGC;
+    } else {
+      key = AVGF;
+    }
+    last_time = key;
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    Tuplet value = TupletCString(key, "msg");
+    dict_write_tuplet(iter, &value);
+    app_message_outbox_send();
+}
+
+
+//switch to the resume mode
 void resume() {
    DictionaryIterator *iter;
    app_message_outbox_begin(&iter);
@@ -118,28 +172,126 @@ void topause() {
    app_message_outbox_send();
 }
 
+/* This is called when the up button is clicked */
 void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-   resume();
+//    resume();
+//   if (pause) return;
+  text_layer_set_text_alignment(hello_layer, GTextAlignmentCenter);
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  mode2++;
+  mode2 = mode2 % number_of_mode2;
+  if (mode2 == 1) {
+    topause();
+  } 
+  else {
+    resume();
+  }
 }
 
+// switch to the polling mode
+void polling() {
+   DictionaryIterator *iter;
+   app_message_outbox_begin(&iter);
+   int key = POLL;
+   pause = 0;
+   last_time = key;
+   Tuplet value = TupletCString(key, "msg");
+   dict_write_tuplet(iter, &value);
+   app_message_outbox_send();
+}
+
+/* This called when the up button is long pressed. */
+void up_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text_alignment(hello_layer, GTextAlignmentCenter);
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  mode3++;
+  mode3 = mode3 % number_of_mode3;
+  if (mode3 == 1) {
+    ispolling = 1;
+    polling();
+    app_timer_register(sleeptime, timer_callback, NULL);
+  } 
+  else {
+    ispolling = 0;
+  }
+}
+
+// /* This called when the up button is double clicked. */
+// void up_double_click_handler(ClickRecognizerRef recognizer, void *context) {
+//     int key = last_time;
+//     DictionaryIterator *iter;
+//     app_message_outbox_begin(&iter);
+//     Tuplet value = TupletCString(key, "msg");
+//     dict_write_tuplet(iter, &value);
+//     app_message_outbox_send();
+// }
+
+
+//switch to the RGB light mode
+void rgb() {
+   DictionaryIterator *iter;
+   app_message_outbox_begin(&iter);
+   int key = RGB;
+   pause = 0;
+   last_time = key;
+   Tuplet value = TupletCString(key, "msg");
+   dict_write_tuplet(iter, &value);
+   app_message_outbox_send();
+}
+
+/* This is called when the down button is clicked */
 void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-   topause();
+    if (pause) return;
+    text_layer_set_text_alignment(hello_layer, GTextAlignmentCenter);
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    rgb();
 }
 
+/* This called when the down button is long pressed. */
+void down_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+    mode4++;
+    mode4 = mode4 % number_of_mode4;
+    int key = TIMER;
+    if (mode4 == 1) key = TIMER;
+    else key = RESUME;
+    last_time = key;
+    DictionaryIterator *iter;
+    app_message_outbox_begin(&iter);
+    Tuplet value = TupletCString(key, "msg");
+    dict_write_tuplet(iter, &value);
+    app_message_outbox_send();
+}
+
+// /* This called when the down button is double clicked. */
+// void down_double_click_handler(ClickRecognizerRef recognizer, void *context) {
+//     int key = last_time;
+//     DictionaryIterator *iter;
+//     app_message_outbox_begin(&iter);
+//     Tuplet value = TupletCString(key, "msg");
+//     dict_write_tuplet(iter, &value);
+//     app_message_outbox_send();
+// }
 
 /* this registers the appropriate function to the appropriate button */
 void config_provider(void *context) {
    window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
    window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
    window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+   window_long_click_subscribe(BUTTON_ID_SELECT, 700, NULL, select_long_click_handler);
+   window_long_click_subscribe(BUTTON_ID_UP, 700, NULL, up_long_click_handler);
+   window_long_click_subscribe(BUTTON_ID_DOWN, 700, NULL, down_long_click_handler);
 }
 
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  hello_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, 80 } });
-  text_layer_set_text(hello_layer, "Welcome! Press the select button to choose mode");
+  hello_layer = text_layer_create((GRect) { .origin = { 0, 40 }, .size = { bounds.size.w, 80 } });
+  text_layer_set_text(hello_layer, "Welcome! Press the button and have fun!");
+  text_layer_set_font(hello_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   text_layer_set_text_alignment(hello_layer, GTextAlignmentCenter);
   text_layer_set_overflow_mode(hello_layer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(hello_layer));
@@ -183,3 +335,4 @@ int main(void) {
   app_event_loop();
   deinit();
 }
+
